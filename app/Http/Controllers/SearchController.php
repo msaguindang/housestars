d95d06c777bf566b08bb9ca211ba7c4e0f12645f
Prinz Nikko <jo_canoy@MacPrinz.local>
2017-03-01 11:55:51 +0800

    hotfix: division by zero

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -160,8 +160,10 @@ class SearchController extends Controller
         $average = 0;
         $numRatings = count($ratings);
 
-        foreach ($ratings as $rating) {
-            $average = ($average + (int)round(($rating->communication + $rating->work_quality + $rating->price + $rating->punctuality + $rating->attitude) / 5)) / $numRatings;
+        if($numRatings > 0){
+            foreach ($ratings as $rating) {
+                $average = ($average + (int)round(($rating->communication + $rating->work_quality + $rating->price + $rating->punctuality + $rating->attitude) / 5)) / $numRatings;
+            }
         }
 
         return $average;
