d48cb6f020de0bfebc5487a978122b1dd719b93d
Prinz Nikko <jo_canoy@MacPrinz.local>
2017-03-03 15:19:23 +0800

    added: homepage advert placement

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,6 +9,7 @@ use App\Suburbs;
 use App\Reviews;
 use App\Role;
 use App\UserMeta;
+use App\Advertisement;
 use Response;
 use Mail;
 
@@ -38,6 +39,47 @@ class MainController extends Controller
         } else { 
             return redirect(env('APP_URL'));
         }
+    }
+
+    function home(){
+
+        $ads = Advertisement::get();
+        // $numAds =  count($ads['728x90'] );
+        // $index = rand(0, $numAds);
+        $x = 0; $z = 0; $y = 0;
+
+        foreach ($ads  as $ad) {
+            switch ($ad->type) {
+                case '728x90':
+                    $advert[$ad->type][$x]['url'] = $ad->image_path;
+                    $x++;
+                    break;
+                case '141x117':
+                    $advert[$ad->type][$z]['url'] = $ad->image_path;
+                    $z++;
+                    break;
+                default:
+                    $advert[$ad->type][$y]['url'] = $ad->image_path;
+                    $y++;
+                    break;
+            }
+            
+        }
+
+        $data = array();
+
+        if(isset($advert['141x117'])){
+            $numAds =  count($advert['141x117']) - 1;
+            $index = rand(0, $numAds);
+            $data['141x117'] = $advert['141x117'][$index ];
+
+        } else if(isset($advert['728x90'])){
+            $numAds =  count($advert['728x90']) - 1;
+            $index = rand(0, $numAds);
+            $data['728x90'] = $advert['728x90'][$index ]; 
+        }
+
+        return view('home')->with('advert', $data);
     }
 
     function agency(){
