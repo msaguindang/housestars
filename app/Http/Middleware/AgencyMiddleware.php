fdbaf1888be051b5f848e54669f574c9c196642c
Prinz Nikko <jo_canoy@MacPrinz.local>
2017-03-14 12:40:19 +0800

    hotfix: unpaid payments

--- app/Http/Middleware/AgencyMiddleware.php
+++ app/Http/Middleware/AgencyMiddleware.php
@@ -21,13 +21,14 @@ class AgencyMiddleware
 
             switch (Sentinel::getUser()->roles()->first()->slug){
                 case 'agency':
+                if(Sentinel::getUser()->customer_id){
+                  \Stripe\Stripe::setApiKey("sk_test_qaq6Jp8wUtydPSmIeyJpFKI1");
+                  $customer_info = \Stripe\Customer::retrieve(Sentinel::getUser()->customer_id);
+                  $payment_status = $customer_info->subscriptions->data[0]->status;
 
-                \Stripe\Stripe::setApiKey("sk_test_qaq6Jp8wUtydPSmIeyJpFKI1");
-                $customer_info = \Stripe\Customer::retrieve(Sentinel::getUser()->customer_id);
-                $payment_status = $customer_info->subscriptions->data[0]->status;
-
-                if($payment_status ==  'past_due' || $payment_status ==  'canceled' || $payment_status ==  'unpaid'){
-                	return redirect('/payment-status');
+                  if($payment_status ==  'past_due' || $payment_status ==  'canceled' || $payment_status ==  'unpaid'){
+                    return redirect('/payment-status');
+                  }
                 }
 
                 return $next($request);
