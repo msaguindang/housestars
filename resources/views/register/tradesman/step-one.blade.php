6ba02e2e54218a810f05e0c4c3b360edb82ec867
Christopher Cinco <christopher.cinco@goabroad.com>
2017-05-04 20:16:13 +0800

    step 1 tradie sign up text edits

--- resources/views/register/tradesman/step-one.blade.php
+++ resources/views/register/tradesman/step-one.blade.php
@@ -84,7 +84,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 form-box tradesman" style="padding: 40px 25px;">
-                    <h2>Tradesman Registration Form</h2>
+                    <h2>Trade/Srevice Registration Form</h2>
                     <form name="step_one_form" action="{{env('APP_URL')}}/add-info" method="POST">
                         @if(session('error'))
                             <div class="alert alert-danger">
@@ -115,10 +115,8 @@
                                 <input type="text" name="website" class="required-input" required>
                                 <label>ABN <span class="required-symbol">*</span></label>
                                 <input type="text" name="abn" class="required-input" required>
-                                <label>Normal Charge Rate <span class="required-symbol">*</span></label>
+                                <label>Standard Hourly Rate (Enter N/A if this does not apply to you) <span class="required-symbol">*</span></label>
                                 <input type="text" name="charge-rate" class="required-input" required>
-
-
                             </div>
                             <label>Write Business Description</label>
                             <textarea placeholder="" class="summary" name="summary"></textarea>
@@ -136,199 +134,6 @@
                                             <label for="t{{$index+1}}">{{ $cat->category }}</label>
                                         </li>
                                     @endforeach
-                                    <!-- <li>
-                                        <input type="radio" id="t1" name="trade"
-                                               value="Architectural Glass and Metal Technician">
-                                        <label for="t1">Architectural Glass and Metal Technician</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t2" name="trade" value="Brick and Stone Mason">
-                                        <label for="t2">Brick and Stone Mason</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t3" name="trade" value="Cement (Concrete) Finisher">
-                                        <label for="t3">Cement (Concrete) Finisher</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t4" name="trade" value="Cement Mason">
-                                        <label for="t4">Cement Mason</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t5" name="trade" value="Concrete Pump Operator">
-                                        <label for="t5">Concrete Pump Operator</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t6" name="trade" value="Construction Boilermaker">
-                                        <label for="t6">Construction Boilermaker</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t7" name="trade" value="Construction Craft Worker">
-                                        <label for="t7">Construction Craft Worker</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t8" name="trade" value="Construction Millwright">
-                                        <label for="t8">Construction Millwright</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t9" name="trade"
-                                               value="Drywall, Acoustic and Lathing Applicator">
-                                        <label for="t9">Drywall, Acoustic and Lathing Applicator</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t10" name="trade"
-                                               value="Drywall Finisher and Plasterer">
-                                        <label for="t10">Drywall Finisher and Plasterer</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t11" name="trade"
-                                               value="Electrician - Construction and Maintenance">
-                                        <label for="t11">Electrician - Construction and Maintenance</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t12" name="trade"
-                                               value="Electrician - Domestic and Rural">
-                                        <label for="t12">Electrician - Domestic and Rural</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t13" name="trade"
-                                               value="Exterior Insulated Finish Systems Mechanic">
-                                        <label for="t13">Exterior Insulated Finish Systems Mechanic</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t14" name="trade" value="Floor Covering Installer">
-                                        <label for="t14">Floor Covering Installer</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t15" name="trade" value="General Carpenter">
-                                        <label for="t15">General Carpenter</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t16" name="trade" value="Hazardous Materials Worker">
-                                        <label for="t16">Hazardous Materials Worker</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t17" name="trade" value="Heat and Frost Insulator">
-                                        <label for="t17">Heat and Frost Insulator</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t18" name="trade"
-                                               value="Heavy Equipment Operator - Dozer">
-                                        <label for="t18">Heavy Equipment Operator - Dozer</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t19" name="trade"
-                                               value="Heavy Equipment Operator - Excavator">
-                                        <label for="t19">Heavy Equipment Operator - Excavator</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t20" name="trade"
-                                               value="Heavy Equipment Operator - Tractor Loader Backhoe">
-                                        <label for="t20">Heavy Equipment Operator - Tractor Loader Backhoe</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t21" name="trade"
-                                               value="Hoisting Engineer - Mobile Crane Operator 1">
-                                        <label for="t21">Hoisting Engineer - Mobile Crane Operator 1</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t22" name="trade"
-                                               value="Hoisting Engineer - Mobile Crane Operator 2">
-                                        <label for="t22">Hoisting Engineer - Mobile Crane Operator 2</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t23" name="trade"
-                                               value="Hoisting Engineer - Tower Crane Operator">
-                                        <label for="t23">Hoisting Engineer - Tower Crane Operator</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t24" name="trade" value="Ironworker - Generalist">
-                                        <label for="t24">Ironworker - Generalist</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t25" name="trade"
-                                               value="Ironworker - Structural and Ornamental">
-                                        <label for="t25">Ironworker - Structural and Ornamental</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t26" name="trade"
-                                               value="Native Residential Construction Worker">
-                                        <label for="t26">Native Residential Construction Worker</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t27" name="trade"
-                                               value="Painter and Decorator - Commercial and Residential">
-                                        <label for="t27">Painter and Decorator - Commercial and Residential</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t28" name="trade"
-                                               value="Painter and Decorator - Industrial">
-                                        <label for="t28">Painter and Decorator - Industrial</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t29" name="trade" value="Plumber">
-                                        <label for="t29">Plumber</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t30" name="trade" value="Powerline Technician">
-                                        <label for="t30">Powerline Technician</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t31" name="trade" value="Precast Concrete Erector">
-                                        <label for="t31">Precast Concrete Erector</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t32" name="trade" value="Precast Concrete Finisher">
-                                        <label for="t32">Precast Concrete Finisher</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t33" name="trade" value="Refractory Mason">
-                                        <label for="t33">Refractory Mason</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t34" name="trade"
-                                               value="Refrigeration and Air Conditioning Systems Mechanic">
-                                        <label for="t34">Refrigeration and Air Conditioning Systems Mechanic</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t35" name="trade" value="Reinforcing Rodworker">
-                                        <label for="t35">Reinforcing Rodworker</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t36" name="trade"
-                                               value="Residential Air Conditioning Systems Mechanic ">
-                                        <label for="t36">Residential Air Conditioning Systems Mechanic </label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t37" name="trade"
-                                               value="Residential (Low Rise) Sheet Metal Installer">
-                                        <label for="t37">Residential (Low Rise) Sheet Metal Installer</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t38" name="trade" value="Restoration Mason">
-                                        <label for="t38">Restoration Mason</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t39" name="trade" value="Roofer">
-                                        <label for="t39">Roofer</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t39" name="trade" value="Sheet Metal Worker">
-                                        <label for="t39">Sheet Metal Worker</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t40" name="trade"
-                                               value="Sprinkler and Fire Protection Installer">
-                                        <label for="t40">Sprinkler and Fire Protection Installer</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t41" name="trade" value="Steamfitter">
-                                        <label for="t41">Steamfitter</label>
-                                    </li>
-                                    <li>
-                                        <input type="radio" id="t42" name="trade"
-                                               value="Terrazzo, Tile and Marble Setter">
-                                        <label for="t42">Terrazzo, Tile and Marble Setter</label>
-                                    </li> -->
                                 </ul>
                             </div>
                             <label>Promotion Code</label>
