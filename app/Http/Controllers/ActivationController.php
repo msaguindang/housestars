a827d6cab91b95d477cbcc4f2aff4b6147131af3
Christopher Cinco <christopher.cinco@goabroad.com>
2017-05-05 20:26:52 +0800

    fixed search issue on admin suburb section;

--- app/Http/Controllers/ActivationController.php
+++ app/Http/Controllers/ActivationController.php
@@ -24,13 +24,14 @@ class ActivationController extends Controller
 
              switch ($account) {
                 case 'agency':
-                return redirect(env('APP_URL').'/register/agency/step-one');
+                    return redirect(env('APP_URL').'/register/agency/step-one');
                 break;
                 case 'tradesman':
-                return redirect(env('APP_URL').'/register/tradesman/step-one');
+                    // sends email
+                    return redirect(env('APP_URL').'/register/tradesman/step-one');
                 break;
                 case 'customer':
-                return redirect(env('APP_URL').'/register/customer/step-one');
+                    return redirect(env('APP_URL').'/register/customer/step-one');
                 break;
             }
         } else {
