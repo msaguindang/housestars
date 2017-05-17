790db8c80e389c2ab095fa455e8703dfadad2681
Prinz Nikko <jo_canoy@MacPrinz.local>
2017-05-17 12:14:41 +0800

    hotfix: selective rate service modal bug

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1268,6 +1268,7 @@
 			maxItems: 1,
 	        openOnFocus: true,
 	        placeholder: "Name of the business",
+	        dropdownParent: 'body',
 			render: {
 				option: function(item, escape) {
 					return "<option class='item' id="+item.value+" value="+item.value+"> "+item.text+"</option>";
